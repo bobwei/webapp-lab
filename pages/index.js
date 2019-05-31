4f89ae68387cfb05243581420cf085ff7b5da406
Bob Wei <bob.wei@linecorp.com>
2019-05-31 17:43:08 +0800

    Add photos layout

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,50 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const fn = () => {
-  return <div>Hello World3</div>;
+const fn = ({ photos }) => {
+  return (
+    <div className="container">
+      <div className="photos-container">
+        {photos.map(({ src }) => (
+          <div
+            key={src}
+            className="photo"
+            style={{ backgroundImage: `url(${src})` }}
+          />
+        ))}
+      </div>
+      <style jsx>
+        {`
+          .container {
+            padding: 0;
+          }
+          .photos-container {
+            margin: 0 auto;
+            max-width: 375px;
+          }
+          @media (min-width: 768px) {
+            .photos-container {
+              margin-top: 50px;
+            }
+          }
+          .photo {
+            display: inline-block;
+            width: 33.33%;
+            padding-bottom: 33.33%;
+            background-repeat: no-repeat;
+            background-size: cover;
+            background-position: 50%;
+            margin-bottom: -6px;
+          }
+        `}
+      </style>
+    </div>
+  );
+};
+
+fn.defaultProps = {
+  photos: require('../src/fixtures/photos.json'),
 };
 
 export default fn;
