556de901d3f8d75019d26df1251a5f2cbc5d5faa
Bob Wei <bob.wei@linecorp.com>
2019-06-01 19:08:25 +0800

    Add navigation bar

--- pages/index.js
+++ pages/index.js
@@ -1,45 +1,53 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar, NavbarBrand, Container } from 'reactstrap';
 
 const fn = ({ photos }) => {
   return (
-    <div className="container">
-      <div className="photos-container">
-        {photos.map(({ src }) => (
-          <div
-            key={src}
-            className="photo"
-            style={{ backgroundImage: `url(${src})` }}
-          />
-        ))}
-      </div>
-      <style jsx>
-        {`
-          .container {
-            padding: 0;
-          }
-          .photos-container {
-            margin: 0 auto;
-            max-width: 375px;
-          }
-          @media (min-width: 768px) {
-            .photos-container {
-              margin-top: 50px;
+    <>
+      <Navbar color="light" light expand="md">
+        <Container>
+          <NavbarBrand href="/">Project</NavbarBrand>
+        </Container>
+      </Navbar>
+      <div className="container">
+        <div className="photos-container">
+          {photos.map(({ src }) => (
+            <div
+              key={src}
+              className="photo"
+              style={{ backgroundImage: `url(${src})` }}
+            />
+          ))}
+        </div>
+        <style jsx>
+          {`
+            .container {
+              padding: 0;
             }
-          }
-          .photo {
-            display: inline-block;
-            width: 33.33%;
-            padding-bottom: 33.33%;
-            background-repeat: no-repeat;
-            background-size: cover;
-            background-position: 50%;
-            margin-bottom: -6px;
-          }
-        `}
-      </style>
-    </div>
+            .photos-container {
+              margin: 0 auto;
+              max-width: 375px;
+            }
+            @media (min-width: 768px) {
+              .photos-container {
+                margin-top: 50px;
+              }
+            }
+            .photo {
+              display: inline-block;
+              width: 33.33%;
+              padding-bottom: 33.33%;
+              background-repeat: no-repeat;
+              background-size: cover;
+              background-position: 50%;
+              margin-bottom: -6px;
+            }
+          `}
+        </style>
+      </div>
+    </>
   );
 };
 
