d78129fd2d3ad5220152ed9c0fb0a4b82fcb79a3
Bob Wei <bob.wei@linecorp.com>
2019-06-01 23:16:18 +0800

    Update layout

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,49 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Navbar, NavbarBrand, Container } from 'reactstrap';
+import { Container, Row, Col } from 'reactstrap';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import { Form, FormGroup, Input } from 'reactstrap';
 
 const fn = ({ photos }) => {
   return (
     <>
       <Navbar color="light" light expand="md">
         <Container>
-          <NavbarBrand href="/">Project</NavbarBrand>
+          <NavbarBrand href="/">Instagram</NavbarBrand>
         </Container>
       </Navbar>
-      <div className="container">
-        <div className="photos-container">
-          {photos.map(({ src }) => (
-            <div
-              key={src}
-              className="photo"
-              style={{ backgroundImage: `url(${src})` }}
-            />
-          ))}
-        </div>
+      <Container>
+        <Row>
+          <Col md={{ size: 6, offset: 3 }}>
+            <div className="search-form">
+              <Form>
+                <FormGroup>
+                  <Input
+                    type="text"
+                    name="search"
+                    id="search"
+                    placeholder="Search with username"
+                    autoFocus
+                  />
+                </FormGroup>
+              </Form>
+            </div>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={{ size: 6, offset: 3 }}>
+            <div className="photos-container">
+              {photos.map(({ src }) => (
+                <div
+                  key={src}
+                  className="photo"
+                  style={{ backgroundImage: `url(${src})` }}
+                />
+              ))}
+            </div>
+          </Col>
+        </Row>
         <style jsx>
           {`
             .container {
@@ -28,11 +51,10 @@ const fn = ({ photos }) => {
             }
             .photos-container {
               margin: 0 auto;
-              max-width: 375px;
             }
             @media (min-width: 768px) {
               .photos-container {
-                margin-top: 50px;
+                margin: 50px 0;
               }
             }
             .photo {
@@ -44,9 +66,17 @@ const fn = ({ photos }) => {
               background-position: 50%;
               margin-bottom: -6px;
             }
+            .search-form {
+              margin-top: 1rem;
+            }
+            @media (min-width: 768px) {
+              .search-form {
+                margin-top: 50px;
+              }
+            }
           `}
         </style>
-      </div>
+      </Container>
     </>
   );
 };
