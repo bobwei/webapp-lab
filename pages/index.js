05790f09d1095e457540f666e5d38a1f57c8a6f7
Bob Wei <bob.wei@linecorp.com>
2019-06-02 15:32:19 +0800

    Load default option on mounted

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Row, Col } from 'reactstrap';
 import { Navbar, NavbarBrand } from 'reactstrap';
@@ -13,11 +13,18 @@ import CarouselView from '../src/components/CarouselView';
 import './index.css';
 
 const Comp = () => {
-  const [photos, setPhotos] = useState([]);
-  const [options, setOptions] = useState(require('../src/fixtures/users.json'));
-  const [query, setQuery] = useState(options.length ? options[0] : null);
+  const [photos, setPhotos] = useState(null);
+  const [options, setOptions] = useState(null);
+  const [query, setQuery] = useState(null);
   const onInputChange = createOnInputChange({ setOptions });
   const onOptionChange = createOnOptionChange({ setQuery, setPhotos });
+  useEffect(() => {
+    const defaultOptions = require('../src/fixtures/users.json');
+    if (!options && !photos) {
+      setOptions(defaultOptions);
+      onOptionChange(defaultOptions[0]);
+    }
+  });
   return (
     <>
       <Navbar color="light" light expand="md">
@@ -29,23 +36,25 @@ const Comp = () => {
         <Row>
           <Col md={{ size: 6, offset: 3 }}>
             <div className="search-form">
-              <Form>
-                <FormGroup>
-                  <Select
-                    autoFocus
-                    options={options}
-                    value={query}
-                    onChange={onOptionChange}
-                    onInputChange={onInputChange}
-                  />
-                </FormGroup>
-              </Form>
+              {options && (
+                <Form>
+                  <FormGroup>
+                    <Select
+                      autoFocus
+                      options={options}
+                      value={query}
+                      onChange={onOptionChange}
+                      onInputChange={onInputChange}
+                    />
+                  </FormGroup>
+                </Form>
+              )}
             </div>
           </Col>
         </Row>
         <Row>
           <Col md={{ size: 6, offset: 3 }}>
-            <CarouselView photos={photos} />
+            {photos && <CarouselView photos={photos} />}
           </Col>
         </Row>
       </Container>
