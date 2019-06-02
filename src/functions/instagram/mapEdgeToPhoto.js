c544627d4f165750ed807b70c90a36cfdb82457f
Bob Wei <bob.wei@linecorp.com>
2019-06-02 16:40:03 +0800

    Add location and caption to mapper

--- src/functions/instagram/mapEdgeToPhoto.js
+++ src/functions/instagram/mapEdgeToPhoto.js
@@ -2,6 +2,15 @@ import * as R from 'ramda';
 
 const fn = R.applySpec({
   src: R.path(['node', 'thumbnail_src']),
+  caption: R.path([
+    'node',
+    'edge_media_to_caption',
+    'edges',
+    0,
+    'node',
+    'text',
+  ]),
+  location: R.path(['node', 'location']),
 });
 
 export default fn;
