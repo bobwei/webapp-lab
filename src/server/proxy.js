e12918dddaa9c7a7e9048b5d2e80450bd75794a1
Bob Wei <bob.wei@linecorp.com>
2019-06-03 15:09:57 +0800

    Refactor proxy to allow multiple entries

--- src/server/proxy.js
+++ src/server/proxy.js
@@ -2,13 +2,20 @@ const express = require('express');
 const proxy = require('http-proxy-middleware');
 
 const proxyConfig = {
-  target: 'https://www.instagram.com',
-  pathRewrite: { '^/proxy': '/' },
-  changeOrigin: true,
+  '/proxy': {
+    target: 'https://www.instagram.com',
+    pathRewrite: { '^/proxy': '/' },
+    changeOrigin: true,
+  },
 };
 
 const app = express();
 
-app.use(proxy(proxyConfig));
+// prettier-ignore
+Object
+  .entries(proxyConfig)
+  .reduce((acc, [key, config]) => {
+    return acc.use(key, proxy(config));
+  }, app)
 
 module.exports = app;
