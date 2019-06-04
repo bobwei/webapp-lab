7cba5ed4aed96fb4897f12287e3eebf850933607
Bob Wei <bob.wei@linecorp.com>
2019-06-04 16:44:40 +0800

    Refactor proxy so that it works for all urls

--- src/server/proxy.js
+++ src/server/proxy.js
@@ -1,27 +1,16 @@
 const express = require('express');
 const proxy = require('http-proxy-middleware');
 
-const configs = [
-  [
-    '/proxy/vp',
-    {
-      target: 'https://scontent-tpe1-1.cdninstagram.com',
-      pathRewrite: { '^/proxy/vp': '/vp' },
-      changeOrigin: true,
-    },
-  ],
-  [
-    '/proxy',
-    {
-      target: 'https://www.instagram.com',
-      pathRewrite: { '^/proxy': '/' },
-      changeOrigin: true,
-    },
-  ],
-];
-
 const app = express();
 
-configs.reduce((acc, [key, config]) => acc.use(key, proxy(config)), app);
+app.use('/proxy/:hostname', (req, res, next) => {
+  const { hostname } = req.params;
+  const handler = proxy({
+    target: `https://${hostname}`,
+    pathRewrite: { [`^/proxy/${hostname}`]: '/' },
+    changeOrigin: true,
+  });
+  handler(req, res, next);
+});
 
 module.exports = app;
