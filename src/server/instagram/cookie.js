a52a929a4baf2f49ebd8e90df197c6a727d18d10
Bob Wei <bob.wei@linecorp.com>
2019-06-05 22:51:16 +0800

    Modify cookie only if key exists

--- src/server/instagram/cookie.js
+++ src/server/instagram/cookie.js
@@ -2,17 +2,19 @@
 
 const fn = (proxyRes, req, res) => {
   const key = 'set-cookie';
-  proxyRes.headers[key] = proxyRes.headers[key].map((value) => {
-    return value
-      .replace(' Domain=.instagram.com;', '')
-      .replace(' Domain=instagram.com;', '')
-      .replace(' Domain=.www.instagram.com;', '')
-      .replace(' Domain=www.instagram.com;', '')
-      .replace(' Domain=.i.instagram.com;', '')
-      .replace(' Domain=i.instagram.com;', '')
-      .replace(' Secure', '')
-      .replace(' HttpOnly', '');
-  });
+  if (key in proxyRes.headers) {
+    proxyRes.headers[key] = proxyRes.headers[key].map((value) => {
+      return value
+        .replace(' Domain=.instagram.com;', '')
+        .replace(' Domain=instagram.com;', '')
+        .replace(' Domain=.www.instagram.com;', '')
+        .replace(' Domain=www.instagram.com;', '')
+        .replace(' Domain=.i.instagram.com;', '')
+        .replace(' Domain=i.instagram.com;', '')
+        .replace(' Secure', '')
+        .replace(' HttpOnly', '');
+    });
+  }
 };
 
 module.exports = fn;
