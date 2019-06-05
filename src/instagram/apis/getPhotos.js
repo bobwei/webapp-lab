6eb7f9bba80222fd941357d30bc5b56370221c60
Bob Wei <bob.wei@linecorp.com>
2019-06-05 18:29:51 +0800

    Rename mapEdgeToPhoto to mapToPhoto

--- src/instagram/apis/getPhotos.js
+++ src/instagram/apis/getPhotos.js
@@ -1,7 +1,7 @@
 import * as R from 'ramda';
 import axios from 'axios';
 
-import mapEdgeToPhoto from '../mappers/mapEdgeToPhoto';
+import mapToPhoto from '../mappers/mapToPhoto';
 
 const fn = ({ id } = {}) => {
   const url = `/proxy/www.instagram.com/graphql/query/?query_hash=f2405b236d85e8296cf30347c9f08c2a&variables={"id":"${id}","first":12}`;
@@ -11,7 +11,7 @@ const fn = ({ id } = {}) => {
     .then(
       R.pipe(
         R.path(['data', 'user', 'edge_owner_to_timeline_media', 'edges']),
-        R.map(mapEdgeToPhoto),
+        R.map(mapToPhoto),
       ),
     );
 };
