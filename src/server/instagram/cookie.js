bcbf8474f3f6d0d5dcf6e432a4c54fc235398aec
Bob Wei <bob.wei@linecorp.com>
2019-06-05 17:23:40 +0800

    Rm domain info in cookie

--- src/server/instagram/cookie.js
+++ src/server/instagram/cookie.js
@@ -3,7 +3,15 @@
 const fn = (proxyRes, req, res) => {
   const key = 'set-cookie';
   proxyRes.headers[key] = proxyRes.headers[key].map((value) => {
-    return value.replace(' Domain=.instagram.com;', '');
+    return value
+      .replace(' Domain=.instagram.com;', '')
+      .replace(' Domain=instagram.com;', '')
+      .replace(' Domain=.www.instagram.com;', '')
+      .replace(' Domain=www.instagram.com;', '')
+      .replace(' Domain=.i.instagram.com;', '')
+      .replace(' Domain=i.instagram.com;', '')
+      .replace(' Secure', '')
+      .replace(' HttpOnly', '');
   });
 };
 
