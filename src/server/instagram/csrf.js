067a46ea03c40161704b278db2550ce2235565df
Bob Wei <bob.wei@linecorp.com>
2019-06-05 17:24:54 +0800

    Apply csrf handler only if not GET

--- src/server/instagram/csrf.js
+++ src/server/instagram/csrf.js
@@ -1,6 +1,6 @@
 /* eslint-disable dot-notation */
 const fn = (req) => {
-  if (req.params.hostname === 'www.instagram.com') {
+  if (req.params.hostname === 'www.instagram.com' && req.method !== 'GET') {
     const csrftoken = 'G3ML0Vf5gdEowA4g8LcfH4Fw5ofx88Dx';
     req.headers['cookie'] = `csrftoken=${csrftoken};`;
     req.headers['x-csrftoken'] = csrftoken;
